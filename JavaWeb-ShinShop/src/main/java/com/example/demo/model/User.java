98279411b6b1a8e54ed22955350c773bfb7b9a72
Shin <Shin@Shin>
2025-07-17 22:57:05 +0800

    feat(model): 新增 User model 檔案

---
+++ JavaWeb-ShinShop/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  // 自動產生UserID
+	private int userId;
+	
+	
+}
