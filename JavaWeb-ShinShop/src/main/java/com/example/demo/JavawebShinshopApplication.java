141fcf7cb13775caa91496e32ffdcd02b434d4e5
Shin880410 <qoo28575636@gmail.com>
2025-07-17 21:16:05 +0800

    first commit

---
+++ JavaWeb-ShinShop/src/main/java/com/example/demo/JavawebShinshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebShinshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavawebShinshopApplication.class, args);
+	}
+
+}
