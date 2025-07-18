991c74ba4d18c81b4180274d3286c8d4d5e2ec93
Shin <Shin@Shin>
2025-07-19 00:24:16 +0800

    feat(model): 更新 User model

--- JavaWeb-ShinShop/src/main/java/com/example/demo/model/User.java
+++ JavaWeb-ShinShop/src/main/java/com/example/demo/model/User.java
@@ -1,6 +1,9 @@
 package com.example.demo.model;
 
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,20 +22,29 @@ public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)  // 自動產生UserID
+	@Column(name = "user_id")
 	private int userId;
 	
+	@Column(name = "user_name")
 	private String userName;
 	
+	@Column(name = "user_account")
 	private String userAccount;
 	
 	private String password;
-	
 	private String phone;
 	
-	private String lastLoginTime;
+	@Column(name = "last_login_time")
+	private LocalDateTime lastLoginTime;
 	
-	private String createTime;
-
+	@Column(name = "update_time")
+	private LocalDateTime updateTime;
+	
+	@Column(name = "create_time")
+	private LocalDateTime createTime;
+	
+	@Column(name = "is_deleted")
+	private boolean isDeleted;
 	
 	
 	
