056cdc07f8f6c7506d457786ce224e6987c164c8
Shin <Shin@Shin>
2025-07-19 00:12:46 +0800

    feat(model): 建立 User model

--- JavaWeb-ShinShop/src/main/java/com/example/demo/model/User.java
+++ JavaWeb-ShinShop/src/main/java/com/example/demo/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,10 +14,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)  // 自動產生UserID
 	private int userId;
+	
+	private String userName;
+	
+	private String userAccount;
+	
+	private String password;
+	
+	private String phone;
+	
+	private String lastLoginTime;
+	
+	private String createTime;
+
+	
+	
 	
 	
 }
